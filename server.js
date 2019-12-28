38ac4069be5fbc4ea93f4d701b080ee89f3de90d
Marilyn <marilyncnn@gmail.com>
2019-12-28 01:00:50 -0800

    committing changes

--- server.js
+++ server.js
@@ -26,7 +26,8 @@ const redirectToHTTPS = require('express-http-to-https').redirectToHTTPS;
 const FORECAST_DELAY = 0;
 
 // CODELAB: If running locally, set your Dark Sky API key here
-const API_KEY = process.env.DARKSKY_API_KEY;
+//const API_KEY = process.env.DARKSKY_API_KEY;
+const API_KEY = 'b86484c947df3b78c0456c4408691741';
 const BASE_URL = `https://api.darksky.net/forecast`;
 
 // Fake forecast data used if we can't reach the Dark Sky API
